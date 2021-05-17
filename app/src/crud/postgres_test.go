ae8e0af0d236bbe6c25cc6e9d84ee914a508b073
anuragjha <toanuragjha@gmail.com>
2021-05-16 22:23:45 -0700

    #2: added postgres POC in app

---
+++ app/src/crud/postgres_test.go
@@ -0,0 +1,35 @@
+package crud
+
+import (
+	"testing"
+)
+
+//type User struct {
+//	ID uint
+//
+//	Name    string
+//	Address string
+//	Age     uint
+//
+//	CreatedAt time.Time
+//	UpdatedAt time.Time
+//}
+
+//func init() {
+//	// Get a DB session
+//	dsn := "host=localhost user=postgres password=changeme dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+//	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+//	if err != nil {
+//		fmt.Println("err:", err)
+//	}
+//	// Migrate Schema
+//	err = db.AutoMigrate(User{})
+//	// Create a Record
+//	tx :=
+//
+//
+//}
+
+func TestPostgresCRUD(t *testing.T) {
+
+}
